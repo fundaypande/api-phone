cace3b6a02d030028200b455240023498c85f98f
fundaypande <funday820@gmail.com>
2018-04-05 01:17:53 +0800

    add post api

--- postapi.php
+++ postapi.php
@@ -1,2 +1,31 @@
-//harusnya disini file php untuk post data dari react native ke database lewat file json
-//contoh source codenya ada di file pak rey yang gak sempat difoto agus
+<?php
+
+  include_once('connect.php');
+
+  if($_SERVER['REQUEST_METHOD'] == "POST"){
+   // Get data
+   if(isset($_POST['merek'])){
+     $merek = $_POST['merek'];
+     $nama = $_POST['nama'];
+     $harga = $_POST['harga'];
+   }
+
+   // Insert data into data base
+   $sql = "INSERT INTO handphone(id, nama, merek, harga) VALUES (NULL, '".$nama."', '".$merek."', '".$harga."')";
+   $qur = mysqli_query($conn, $sql);
+   if($qur){
+     $json = array("status" => 1, "msg" => "Done Data Aded!");
+   }else{
+     $json = array("status" => 0, "msg" => "Error adding data!", "merek" => $merek, "nama" => $nama, "harga" => $harga, "sql" => $sql);
+   }
+   }else{
+    $json = array("status" => 0, "msg" => "Request method not accepted");
+  }
+
+  mysqli_close($conn);
+
+  /* Output header */
+   header('Content-type: application/json');
+   echo json_encode($json);
+
+ ?>
